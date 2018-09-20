c1907157558c73515c80fc89bb680a0df3bcdb2d
Albert Kellner Lescinskiene <albert.kellner@gmail.com>
2018-09-20 13:56:48 -0300

    DataBase Name

--- Repository/DesignTimeDbContextFactory.cs
+++ Repository/DesignTimeDbContextFactory.cs
@@ -9,7 +9,7 @@
         {
             var builder = new DbContextOptionsBuilder<OnCareContext>();
 
-            const string connection = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=OnCare;Integrated Security=True;Min Pool Size=0;Max Pool Size=500;Connect Timeout=10;Load Balance Timeout=5;Application Name=OnCareApi";
+            const string connection = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=AspnetTest;Integrated Security=True;Min Pool Size=0;Max Pool Size=500;Connect Timeout=10;Load Balance Timeout=5;Application Name=OnCareApi";
             //const string connection = @"Data Source=vsrv300;Initial Catalog=OnCare;Persist Security Info=True;User ID=sa;Password=K@lunga!;Min Pool Size=0;Max Pool Size=500;Connect Timeout=15;Application Name=OnCare";
 
             builder.UseSqlServer(connection);
