03be6d032c5d76394a8066774f3e88ad88b7063e
Albert Kellner Lescinskiene <albert.kellner@gmail.com>
2020-01-15 00:14:30 -0300

    Inicio modelagem podcast

---
+++ DataEntity/Model/CanalEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace DataEntity.Model
+{
+    class CanalEntity : BaseEntity
+    {
+        public string Nome { get; set; }
+        public string Criador { get; set; }
+        public EpisodioEntity Episodio { get; set; }
+    }
+}
---
+++ DataEntity/Model/EpisodioEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DataEntity.Model
+{
+    class EpisodioEntity : BaseEntity
+    {
+        public int NumeroDoEpisodio { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Playlist { get; set; }
+
+        public DateTime DataPublicacao { get; set; }
+    }
+}
